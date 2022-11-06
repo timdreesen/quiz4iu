ff2f235244aae8e590ccf727bf16caea1d85b29b
timdreesen <tim.dreesen@gmail.com>
2022-11-06 16:56:27 -0500

    Frontend Grundgerüst
     header component
     	-navbar
     		start/fragenkatalog/statistik/registrieren/login
    
     home component
     	-platzhalter
     	-buttons
     		quiz-duell/team-quiz
    
     footer component
     	-platzhalter

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  
+<!-- header -->
+<app-header></app-header>
+
+<app-home> </app-home>
+<!-- routes get injected here  -->
+<router-outlet></router-outlet>
+
+<!-- footer -->
+<app-footer></app-footer>
+`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'isef';
+}
