3b287b806ed3b53c758a7e5fbf9a426e7c39fada
timdreesen <tim.dreesen@gmail.com>
2022-11-28 03:50:39 -0500

    Registrierung übersetzt
    Registrierung übersetzt

--- quiz4iu/quiz4iu/settings.py
+++ quiz4iu/quiz4iu/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['quiz4iudeploytest.azurewebsites.net','127.0.0.1','169.254.130.
 
 CSRF_TRUSTED_ORIGINS = ['https://*.quiz4iudeploytest.azurewebsites.net','https://*.127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -111,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
 TIME_ZONE = 'UTC'
 
